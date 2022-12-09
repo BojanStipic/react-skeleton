8afb4da21d493267eae21b230c583c0b0d782bcb
BojanStipic <bojanstipic@gmail.com>
2022-12-09 15:16:08 +0100

    Setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { Heading } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import { Route, Routes } from "react-router-dom";
 
 export const App = () => {
   const { t } = useTranslation();
 
-  return <Heading>{t("React Skeleton")}</Heading>;
+  return (
+    <Routes>
+      <Route path="/" element={<Heading>{t("React Skeleton")}</Heading>} />
+      <Route path="*" element={<Heading>{t("Page not found")}</Heading>} />
+    </Routes>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
 import { theme } from "./theme";
@@ -10,7 +11,9 @@ import "./i18n";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
 );
