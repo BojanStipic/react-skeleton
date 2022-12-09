693e598995fc57a731b860add41e1ce7d8e48ea6
BojanStipic <bojanstipic@gmail.com>
2022-12-09 16:40:14 +0100

    Prefer type instead of interface

--- src/i18n/@types/i18next.d.ts
+++ src/i18n/@types/i18next.d.ts
@@ -2,6 +2,7 @@ import "i18next";
 import { resources } from "..";
 
 declare module "i18next" {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface CustomTypeOptions {
     resources: typeof resources["en-US"];
   }
